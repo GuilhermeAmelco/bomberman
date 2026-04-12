686a58b337ecbc5ce429bb3341108869153f585d
Guilherme <amelcoguilherme@gmail.com>
2026-04-12 19:42:43 -0300

    separando as funcionalidades e as entidades

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
+#include <string.h>
 
 using namespace std;
 
@@ -19,6 +20,11 @@ const char BLOCO_CLARO = 176;
 const int LARGURA = 15;
 const int ALTURA = 11;
 
+// estados
+const string ESTADO_ATIVA = "ATIVA";
+const string ESTADO_DESATIVADA = "INATIVA";
+const string ESTADO_EXPLODINDO = "EXPLODINDO";
+
 /* =========================
    INPUT
 ========================= */
@@ -35,7 +41,7 @@ int escuta_tecla()
    REGRAS
 ========================= */
 
-bool pode_mover(int mapa[ALTURA][LARGURA], int bomba_posicao[2], int x, int y)
+bool pode_mover(int mapa[ALTURA][LARGURA], int bomba_posicao[2], string bomba_estado, int x, int y)
 {
   if (x < 0 || x >= LARGURA)
     return false;
@@ -46,17 +52,22 @@ bool pode_mover(int mapa[ALTURA][LARGURA], int bomba_posicao[2], int x, int y)
   if (mapa[y][x] == 0 || mapa[y][x] == 1)
     return false;
 
-  if (bomba_posicao[0] == y && bomba_posicao[1] == x)
+  if (bomba_posicao[0] == y && bomba_posicao[1] == x && bomba_estado == ESTADO_ATIVA)
     return false;
 
   return true;
+}
+
+bool bomba_ativa(string bomba_estado)
+{
+  return bomba_estado == ESTADO_ATIVA;
 }
 
 /* =========================
    LÓGICA DAS ENTIDADES
 ========================= */
 
-void mover_jogador(int jogador_posicao[2], int bomba_posicao[2], int mapa[ALTURA][LARGURA], int tecla)
+void mover_jogador(int jogador_posicao[2], int bomba_posicao[2], string bomba_estado, int mapa[ALTURA][LARGURA], int tecla)
 {
   int x = jogador_posicao[1];
   int y = jogador_posicao[0];
@@ -73,16 +84,16 @@ void mover_jogador(int jogador_posicao[2], int bomba_posicao[2], int mapa[ALTURA
       x++;
   }
 
-  if (!pode_mover(mapa, bomba_posicao, x, y))
+  if (!pode_mover(mapa, bomba_posicao, bomba_estado, x, y))
     return;
 
   jogador_posicao[1] = x;
   jogador_posicao[0] = y;
 }
 
-void coloca_bomba(int jogador_posicao[2], int bomba_posicao[2], int mapa[ALTURA][LARGURA], int tecla)
+void coloca_bomba(int jogador_posicao[2], int bomba_posicao[2], string &bomba_estado, int &bomba_tempo, int tecla)
 {
-  if (bomba_posicao[0] >= 0 || bomba_posicao[1] >= 0)
+  if (bomba_ativa(bomba_estado))
     return;
 
   if (tecla)
@@ -91,21 +102,50 @@ void coloca_bomba(int jogador_posicao[2], int bomba_posicao[2], int mapa[ALTURA]
     {
       bomba_posicao[0] = jogador_posicao[0];
       bomba_posicao[1] = jogador_posicao[1];
+      bomba_estado = ESTADO_ATIVA;
+      bomba_tempo = clock();
     }
   }
+}
+
+void explodir_bomba(string &bomba_estado, int &bomba_tempo)
+{
+  int tempo_passado = clock() - bomba_tempo;
+
+  if (tempo_passado < 3000)
+    return; // nao passou 3s
+
+  bomba_estado = ESTADO_DESATIVADA;
+  bomba_tempo = 0;
 }
 
 /* =========================
    RENDER
 ========================= */
 
-void desenhar(int mapa[ALTURA][LARGURA], int jogador_posicao[2], int bomba_posicao[2], HANDLE out)
+void atualiza_jogador(int jogador_posicao[2], int bomba_posicao[2], string &bomba_estado, int &bomba_tempo, int mapa[ALTURA][LARGURA], int tecla)
+{
+  mover_jogador(jogador_posicao, bomba_posicao, bomba_estado, mapa, tecla);
+  coloca_bomba(jogador_posicao, bomba_posicao, bomba_estado, bomba_tempo, tecla);
+}
+
+void atualiza_bomba(int bomba_posicao[2], string &bomba_estado, int &bomba_tempo, int mapa[ALTURA][LARGURA])
+{
+  if (bomba_estado == ESTADO_ATIVA)
+    explodir_bomba(bomba_estado, bomba_tempo);
+}
+
+void desenhar(
+    int mapa[ALTURA][LARGURA],
+    int jogador_posicao[2],
+    int bomba_posicao[2], string bomba_estado,
+    HANDLE out)
 {
   for (int i = 0; i < ALTURA; i++)
   {
     for (int j = 0; j < LARGURA; j++)
     {
-      bool isBomb = (bomba_posicao[0] == i && bomba_posicao[1] == j);
+      bool isBomb = (bomba_posicao[0] == i && bomba_posicao[1] == j && bomba_estado == ESTADO_ATIVA);
       bool isPlayer = (jogador_posicao[0] == i && jogador_posicao[1] == j);
 
       if (isBomb)
@@ -149,10 +189,6 @@ void desenhar(int mapa[ALTURA][LARGURA], int jogador_posicao[2], int bomba_posic
    MAIN
 ========================= */
 
-// logica da bomba:
-// pode ser uma coordenadas so entao
-//
-
 int main()
 {
   // configuração do console
@@ -181,22 +217,47 @@ int main()
       {0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 0},
       {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-  // entidades (y, x)
+  // jogador
   int jogador_posicao[2] = {1, 1};
-  int bomba_posicao[2] = {-1, -1};
+
+  // bomba(s)
+  int bomba_posicao[2];
+  string bomba_estado = ESTADO_DESATIVADA; // pode ser inativa, ativa e explodindo
+  int bomba_tempo = 0;
 
   int tecla;
 
+  // logica tempo
+  const int FPS = 60;
+  int tempo_por_segundo = 1000 / FPS;
+
   while (true)
   {
+    int tempo_inicio = clock();
+
     // limpa tela (reposiciona cursor)
     SetConsoleCursorPosition(out, coord);
 
-    desenhar(mapa, jogador_posicao, bomba_posicao, out);
+    desenhar(
+        mapa,
+        jogador_posicao,
+        bomba_posicao, bomba_estado,
+        out);
 
     tecla = escuta_tecla();
-    mover_jogador(jogador_posicao, bomba_posicao, mapa, tecla);
-    coloca_bomba(jogador_posicao, bomba_posicao, mapa, tecla);
+
+    // funcionalidades do jogador
+    atualiza_jogador(jogador_posicao, bomba_posicao, bomba_estado, bomba_tempo, mapa, tecla);
+
+    // funcionalidades da bomba
+    atualiza_bomba(bomba_posicao, bomba_estado, bomba_tempo, mapa);
+
+    // logica fps
+    int tempo_executado = clock() - tempo_inicio;
+    if (tempo_por_segundo > tempo_executado)
+    {
+      Sleep(tempo_por_segundo - tempo_executado);
+    }
   }
 
   return 0;
